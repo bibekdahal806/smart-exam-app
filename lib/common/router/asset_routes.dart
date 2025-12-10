a5d09a424a0ab8aea08d8d48dcbb837fb1e80134
bibekdahal806 <bibekdahal806@gmail.com>
2025-12-10 21:37:00 +0545

    Add onboarding SVG and clapping sound asset; update asset routes and pubspec.yaml
    - Added onboarding_3.svg to the onboarding SVG assets.
    - Added clapping.wav sound asset.
    - Updated asset_routes.dart to include new asset paths for onboarding images and various icons.
    - Modified pubspec.yaml to include new assets and fonts, ensuring proper asset organization.

--- lib/common/router/asset_routes.dart
+++ lib/common/router/asset_routes.dart
@@ -1,34 +1,57 @@
 // Constants for asset paths to images and icons.
 
+// Constants for asset paths to images and icons.
+
 class AssetRoutes {
   static const String iconPath = 'assets/icons';
   static const String iconSvgPath = '$iconPath/svg';
   static const String iconImagePath = '$iconPath/images';
   static const String imagePath = 'assets/images';
   static const String imageSvgPath = '$imagePath/svg';
+  static const String onboardingImagePath = "$imageSvgPath/onboarding";
   static const String musicPath = 'assets/music';
 
   ///
   ///icons
+  ///iconSvgPath
   static const String arrowBackIconPath = "$iconSvgPath/arrow_back.svg";
   static const String arrowForwardIconPath = "$iconSvgPath/arrow_forward.svg";
   static const String arrowBackIosIconPath = "$iconSvgPath/arrow_back_ios.svg";
   static const String arrowForwardIosIconPath =
       "$iconSvgPath/arrow_forward_ios.svg";
+  static const String languageIconPath = "$iconSvgPath/language.svg";
+  static const String checkIconPath = "$iconSvgPath/check.svg";
+  static const String docsIconPath = "$iconSvgPath/docs.svg";
+  static const String settingIconPath = "$iconSvgPath/setting.svg";
+  static const String deleteIconPath = "$iconSvgPath/delete.svg";
+  static const String closeIconPath = "$iconSvgPath/close.svg";
+  static const String errorFolderIconPath = "$iconSvgPath/error_folder.svg";
+  static const String errorCircleIconPath = "$iconSvgPath/error_circle.svg";
+  static const String qrIconPath = "$iconSvgPath/qr.svg";
+  static const String downloadIconPath = "$iconSvgPath/download.svg";
+  static const String shareIconPath = "$iconSvgPath/share.svg";
+  static const String transferIconPath = "$iconSvgPath/transfer.svg";
+  static const String popupCloseIconPath = "$iconSvgPath/popup_close.svg";
+  static const String topTrophyIconPath = "$iconSvgPath/top_trophy.svg";
+  static const String emailIconPath = "$iconSvgPath/email.svg";
+  static const String facebookIconPath = "$iconSvgPath/facebook.svg";
+  static const String githubIconPath = "$iconSvgPath/github.svg";
+  static const String instagramIconPath = "$iconSvgPath/instagram.svg";
+  static const String linkedinIconPath = "$iconSvgPath/linkedin.svg";
+  static const String websiteIconPath = "$iconSvgPath/website.svg";
   static const String eyeCloseIconPath = "$iconSvgPath/eye_close.svg";
   static const String eyeOpenIconPath = "$iconSvgPath/eye_open.svg";
-  static const String errorFolderIconPath = "$iconSvgPath/error_folder.svg";
 
   ///
   ///images
+  ///imagesPath
   static const String defaultAvatarImagePath = "$imagePath/default_avatar.png";
   static const String defaultPlaceholderImagePath =
       "$imagePath/default_placeholder.png";
 
-  ///images->svg
-  //images-> onboarding
-
-  static const String onboardingImagePath = "$imageSvgPath/onboarding";
+  ///imagesPath
+  ///imageSvgPath
+  ///onboardingImagePath
   static const String onboarding1ImagePath =
       "$onboardingImagePath/onboarding_1.svg";
   static const String onboarding2ImagePath =
