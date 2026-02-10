7d4b705b74c29dc5b25babeae232e180781af64a
bibekdahal806 <bibekdahal806@gmail.com>
2026-02-10 09:13:27 +0545

    feat: refactor the question type

--- lib/modules/exam/features/survey/widgets/answer_choice_widget.dart
+++ lib/modules/exam/features/survey/widgets/answer_choice_widget.dart
@@ -9,9 +9,11 @@ class AnswerChoiceWidget extends StatefulWidget {
   ///The parameter that contains the data pertaining to a question.
   final Question question;
 
-  const AnswerChoiceWidget(
-      {Key? key, required this.question, required this.onChange})
-      : super(key: key);
+  const AnswerChoiceWidget({
+    Key? key,
+    required this.question,
+    required this.onChange,
+  }) : super(key: key);
 
   @override
   State<AnswerChoiceWidget> createState() => _AnswerChoiceWidgetState();
@@ -23,10 +25,14 @@ class _AnswerChoiceWidgetState extends State<AnswerChoiceWidget> {
     if (widget.question.answerChoices.isNotEmpty) {
       if (widget.question.singleChoice) {
         return SingleChoiceAnswer(
-            onChange: widget.onChange, question: widget.question);
+          onChange: widget.onChange,
+          question: widget.question,
+        );
       } else {
         return MultipleChoiceAnswer(
-            onChange: widget.onChange, question: widget.question);
+          onChange: widget.onChange,
+          question: widget.question,
+        );
       }
     } else {
       return SentenceAnswer(
@@ -44,9 +50,11 @@ class SingleChoiceAnswer extends StatefulWidget {
 
   ///The parameter that contains the data pertaining to a question.
   final Question question;
-  const SingleChoiceAnswer(
-      {Key? key, required this.onChange, required this.question})
-      : super(key: key);
+  const SingleChoiceAnswer({
+    Key? key,
+    required this.onChange,
+    required this.question,
+  }) : super(key: key);
 
   @override
   State<SingleChoiceAnswer> createState() => _SingleChoiceAnswerState();
@@ -65,29 +73,30 @@ class _SingleChoiceAnswerState extends State<SingleChoiceAnswer> {
   @override
   Widget build(BuildContext context) {
     return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: widget.question.answerChoices.keys
-            .map((answer) => Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4),
-                  child: Row(
-                    children: [
-                      Radio(
-                          value: answer,
-                          groupValue: _selectedAnswer,
-                          onChanged: (value) {
-                            setState(() {
-                              _selectedAnswer = value as String;
-                            });
-                            widget.onChange([_selectedAnswer!]);
-                          }),
-                      Flexible(
-                        fit: FlexFit.loose,
-                        child: Text(answer),
-                      )
-                    ],
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: widget.question.answerChoices.keys
+          .map(
+            (answer) => Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: Row(
+                children: [
+                  Radio(
+                    value: answer,
+                    groupValue: _selectedAnswer,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedAnswer = value as String;
+                      });
+                      widget.onChange([_selectedAnswer!]);
+                    },
                   ),
-                ))
-            .toList());
+                  Flexible(fit: FlexFit.loose, child: Text(answer)),
+                ],
+              ),
+            ),
+          )
+          .toList(),
+    );
   }
 }
 
@@ -97,9 +106,11 @@ class MultipleChoiceAnswer extends StatefulWidget {
 
   ///The parameter that contains the data pertaining to a question.
   final Question question;
-  const MultipleChoiceAnswer(
-      {Key? key, required this.onChange, required this.question})
-      : super(key: key);
+  const MultipleChoiceAnswer({
+    Key? key,
+    required this.onChange,
+    required this.question,
+  }) : super(key: key);
 
   @override
   State<MultipleChoiceAnswer> createState() => _MultipleChoiceAnswerState();
@@ -118,27 +129,28 @@ class _MultipleChoiceAnswerState extends State<MultipleChoiceAnswer> {
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: widget.question.answerChoices.keys
-            .map((answer) => Row(
-                  children: [
-                    Checkbox(
-                        value: _answers.contains(answer),
-                        onChanged: (value) {
-                          if (value == true) {
-                            _answers.add(answer);
-                          } else {
-                            _answers.remove(answer);
-                          }
-                          widget.onChange(_answers);
-                          setState(() {});
-                        }),
-                    Flexible(
-                      fit: FlexFit.loose,
-                      child: Text(answer),
-                    )
-                  ],
-                ))
-            .toList());
+      children: widget.question.answerChoices.keys
+          .map(
+            (answer) => Row(
+              children: [
+                Checkbox(
+                  value: _answers.contains(answer),
+                  onChanged: (value) {
+                    if (value == true) {
+                      _answers.add(answer);
+                    } else {
+                      _answers.remove(answer);
+                    }
+                    widget.onChange(_answers);
+                    setState(() {});
+                  },
+                ),
+                Flexible(fit: FlexFit.loose, child: Text(answer)),
+              ],
+            ),
+          )
+          .toList(),
+    );
   }
 }
 
@@ -148,9 +160,11 @@ class SentenceAnswer extends StatefulWidget {
 
   ///The parameter that contains the data pertaining to a question.
   final Question question;
-  const SentenceAnswer(
-      {Key? key, required this.onChange, required this.question})
-      : super(key: key);
+  const SentenceAnswer({
+    Key? key,
+    required this.onChange,
+    required this.question,
+  }) : super(key: key);
 
   @override
   State<SentenceAnswer> createState() => _SentenceAnswerState();
